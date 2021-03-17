3087b6271e6505e02b0f3bde8b432f4fbc61660a
TheOldLittleBoy <18672542272@163.com>
2021-03-17 14:31:36 +0800

    增加监听

--- app/src/main/java/com.zsl.toggleview/MainActivity.java
+++ app/src/main/java/com.zsl.toggleview/MainActivity.java
@@ -1,12 +1,16 @@
 package com.zsl.toggleview;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.util.Log;
 
+/**
+ * @author zsl
+ */
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +20,7 @@ public class MainActivity extends AppCompatActivity {
         toggleView.setChecked(true)
                 .setData("开","关")
                 .build();
+
+        toggleView.setOnCheckedListener(isChecked -> Log.i(TAG, "onCreate: "+isChecked));
     }
 }
